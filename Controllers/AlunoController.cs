d6af670d2ff7cfe029ee00bfe13eee201cd08ed7
Laboratório FIAP <gabrieldiegues12@gmail.com>
2025-10-02 10:09:55 -0300

    adding api

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        IRepository<Aluno> repo = new AlunoRepository(""); // Colocar a connection string do banco de dados
+        public struct BuscaRequest
+        {
+            public string Proprieadade { get; }
+            public object Valor { get; }
+
+            public BuscaRequest(string Proprieadade, object Valor)
+            {
+                this.Proprieadade = Proprieadade;
+                this.Valor = Valor;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            int id = repo.Inserir(aluno.Nome, aluno.Idade, aluno.Email, aluno.DataNascimento);
+            if(id != 0)
+            {
+                return Ok(id);
+            }
+            return BadRequest("Dados do aluno fornecido estão incorretos");
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(repo.Listar());
+
+
+        [HttpPut]
+        public IActionResult Put(Aluno aluno)
+        {
+            int linhasAfetadas = repo.Atualizar(aluno.Id, aluno.Nome, aluno.Idade, aluno.Email, aluno.DataNascimento);
+            if(linhasAfetadas > 0)
+            {
+                return Ok(aluno);
+            }
+            return BadRequest("Não foi possível atualizar o aluno");
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            int linhasAfetadas = repo.Excluir(id);
+            if(linhasAfetadas > 0)
+            {
+                return Ok(id);
+            }
+            return BadRequest("Não foi possível deletar o aluno");
+        }
+
+
+        [HttpPost]
+        public IActionResult GetAlunos(BuscaRequest busca) => Ok(repo.Buscar(busca.Proprieadade, busca.Valor));
+    }
+}
